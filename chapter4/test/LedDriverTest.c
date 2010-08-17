e3d23e5525dfc2bef9b5bd6e0e60b88babbbfd0b
Takeshi UME Imai <takeshi@umejam.org>
2010-08-17 12:06:25 +0900

    code in page 75. now, build is failed, just as expected.
    	* chapter4/test/LedDriverTest.c (test_LedsOffAfterInitialization):
    	use virtualLeds variable representing the virtual LEDs.
    	call LedDriver_Create to initialize.

--- chapter4/test/LedDriverTest.c
+++ chapter4/test/LedDriverTest.c
@@ -1,4 +1,5 @@
 #include <cutter.h>
+#include <stdint.h>
 
 void cut_setup(void)
 {
@@ -10,5 +11,7 @@ void cut_teardown(void)
 
 void test_LedsOffAfterInitialization(void)
 {
-  cut_fail("Start here");
+  uint16_t virtualLeds = 0xffff;
+  LedDriver_Create(&virtualLeds);
+  cut_assert_equal_uint_least16(0, virtualLeds);
 }
