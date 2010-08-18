4f4163ff30c80a740f11285804f64c31331715ce
Takeshi UME Imai <takeshi@umejam.org>
2010-08-18 09:55:35 +0900

    on page 81. add new test. build is failed.
    	* chapter4/test/LedDriverTest.c (test_TurnOnLedOn): add new test
    	code. test LedDriver_TurnOn.

--- chapter4/test/LedDriverTest.c
+++ chapter4/test/LedDriverTest.c
@@ -15,3 +15,11 @@ void test_LedsOffAfterInitialization(void)
   LedDriver_Create(&virtualLeds);
   cut_assert_equal_uint_least16(0, virtualLeds);
 }
+
+void test_TurnOnLedOn(void)
+{
+  uint16_t virtualLeds;
+  LedDriver_Create(&virtualLeds);
+  LedDriver_TurnOn(1);
+  cut_assert_equal_uint_least16(1, virtualLeds);
+}
