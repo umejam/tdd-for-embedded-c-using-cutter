3ff43232b88bcdd527c61956abce67331f417cdd
Takeshi UME Imai <takeshi@umejam.org>
2010-08-18 10:00:46 +0900

    in page 83. build is ok but test is failed.
    	* chapter4/src/LedDriver.h (LedDriver_TurnOn): add declaration.
    
    	* chapter4/src/LedDriver.c (LedDriver_TurnOn): add function but
    	not implement yet.

--- chapter4/src/LedDriver.c
+++ chapter4/src/LedDriver.c
@@ -5,4 +5,9 @@ void LedDriver_Create(uint16_t* address)
   *address = 0;
 }
 
+void LedDriver_TurnOn(int ledNumber)
+{
+}
+
+
 
--- chapter4/src/LedDriver.h
+++ chapter4/src/LedDriver.h
@@ -4,5 +4,6 @@
 #include <stdint.h>
 
 void LedDriver_Create(uint16_t* address);
+void LedDriver_TurnOn(int ledNumber);
 
 #endif
