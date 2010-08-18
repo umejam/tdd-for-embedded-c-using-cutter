1107212effddf9587bd1baea16df3d0ae2bf8ab4
Takeshi UME Imai <takeshi@umejam.org>
2010-08-18 10:43:18 +0900

    on page 85. test is passed but implementation is wrong.
    	* chapter4/src/LedDriver.c (LedDriver_Create): store LED address
    	to static variable.
    	(LedDriver_TurnOn): set 1.

--- chapter4/src/LedDriver.c
+++ chapter4/src/LedDriver.c
@@ -1,12 +1,16 @@
 #include "LedDriver.h"
 
+static uint16_t* ledAddress;
+
 void LedDriver_Create(uint16_t* address)
 {
-  *address = 0;
+  ledAddress = address;
+  *ledAddress = 0;
 }
 
 void LedDriver_TurnOn(int ledNumber)
 {
+  *ledAddress = 1;
 }
 
 
